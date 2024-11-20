cde21fb6c322d5a03a3b8330e2a58995952b2957
Santino Casado <santinocasado05@gmail.com>
2024-11-20 18:57:03 -0300

    del: functions in Validate

--- Funciones/Validate.py
+++ Funciones/Validate.py
@@ -5,14 +5,6 @@ def validate_number(dato: int) -> bool:
     except ValueError:
         return  False
 
-def validate_lenght(str,lenght):
-    try:
-        if len(str) <= lenght:
-            return str
-        
-    except ValueError:
-        return None
-
 def get_int(mensaje: str, mensaje_error: str, minimo: int, maximo: int, reintentos: int) -> (int | None):
     dato_ingresado = input(mensaje)
     
@@ -36,30 +28,7 @@ def get_int(mensaje: str, mensaje_error: str, minimo: int, maximo: int, reintent
         else:
             return None
 
-def get_str(mensaje:str,mensaje_error:str, longitud:int) -> str|None: 
-    user_input = str(input(mensaje))
-    string = validate_lenght(user_input,longitud)
-    if string is not None:
-        return string
-    else: 
-        print(mensaje_error)
-    
-    return None
 
-def validar_entero(numero):
-    while True:
-        try:
-            valor = int(input(numero))
-            return valor
-        except ValueError:
-            print("Error: Ingrese un número entero válido.")
-
-def validar_lista_existente(lista):
-    if lista == []:
-        return True
-    else:
-        return False
-    
 def validar_letra (letra, palabra):
     for i in range (len(palabra)):
         if letra == palabra[i]:
